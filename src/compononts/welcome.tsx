ad34b801cd9c758f46327a402b903d0d15f0f7cc
ahmed-rmdan <ahmedrmadan251998@gmail.com>
2025-08-03 04:29:28 +0300

    welcome ui

--- src/compononts/welcome.tsx
+++ src/compononts/welcome.tsx
@@ -3,10 +3,10 @@ import fationimg from '../assets/young-stylish-confident-happy-handsome-business
 import { NavLink } from "react-router-dom";
 export const Welcome:React.FC<{}>=()=>{
     return(
-           <div className="container flex flex-row text-black h-[900px]  mx-auto text-[2.5px] sm:text-[4px] md:text-[4px] lg:text-[6px] xl:text-[7px] ">
-                <div className="flex w-[50%] items-center justify-center flex-col gap-[60px] ">
+           <div className="container flex flex-row text-black h-[650px]  mx-auto  text-[2.5px] md:text-[4.5px] md:h-[900px] lg:text-[5px] xl:text-[7px] ">
+                <div className="flex w-[50%] items-center justify-center flex-col gap-[35px] md:gap-[60px] ">
                    <p className="text-center text-[9em] leading-[80px] lg:leading-[100px] ">WELCOME TO OUR STORE <br/>we have <span className="text-purple-800"> clothes</span> </p>
-                  <NavLink to={'/products'}><button className="buttonstyle w-[100px] sm:w-[140px] h-[50px] text-[18px] sm:text-[22px] " >Shop Now!</button></NavLink> 
+                  <NavLink to={'/products'}><button className="buttonstyle w-[7.5em] h-[2.5em] text-[4.6em]   md:w-[7em] md:h-[2em]  md:text-[4em]  md:h-[2.3em]   " >Shop Now!</button></NavLink> 
                 </div>
                 <div className="w-[50%] h-full" >
                     <img src={fationimg} className="w-[100%] h-[100%]">
